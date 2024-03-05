9cb1415878ca10f83dba7c3c073c40d8e5d62b58
LUEJ9 <juan.luethy@coop.ch>
2024-03-05 10:12:45 +0100

    finished something

--- src/ParentComponent.tsx
+++ src/ParentComponent.tsx
@@ -3,23 +3,32 @@ import {response} from "./data.ts";
 import ChildComponent from "./ChildComponent.tsx";
 
 export default function ParentComponent(){
+    const [error, setError] = useState<string>();
     const [character, setCharacter] = useState(response.results);
 
     const filterCharacters = (s: string) => {
-        setCharacter(response.results.filter((d) => d.name.startsWith(s) || d.id == s || d.status.startsWith(s)))
+        const newArr = response.results.filter((d) => d.name.toLowerCase().startsWith(s) || d.id == s);
+        setCharacter(newArr);
+        if (newArr.length === 0) {
+            setError("Nothing found");
+        } else {
+            setError(undefined);
+        }
     }
+
 
     return <div>
 
         <input onChange={(event) => {
-            filterCharacters(event.target.value)
+            filterCharacters(event.target.value.toLowerCase())
         }}/>
+
+        {error && <p>{error}</p>}
 
         {character.map((d) => {
             return <ChildComponent id={d.id} name={d.name}/>
         })}
 
     </div>
-
 
 }
